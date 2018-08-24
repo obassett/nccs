9770d2af93cdb77b86fcb23fab798f93fb9cf13b
Bassett <obassett@netapp.com>
2018-08-24 16:36:18 +1000

    First Project Done - Woot!

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+
+Ollie = 'Awesmoe'
+
+#Check to see if Ollie is awesome
+if Ollie == 'Awesome':
+    # Output Hello World to the screen
+    print('Hello World')
+else:
+    print('Ollie is not Awesome')
+
